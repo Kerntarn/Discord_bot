3e07dc2daf6d940eed31e92d079ae9b4f448bb4b
Kerntarn <fluk032548@gmail.com>
2024-04-09 17:26:53 +0700

    add choices in command

--- src/commands/utility/echo.js
+++ src/commands/utility/echo.js
@@ -4,19 +4,26 @@ module.exports = {
     data: new SlashCommandBuilder()
 	    .setName('echo')
 	    .setDescription('Replies with your input!')
-        .addChannelOption(option =>
-            option.setName('channel')
-                .setDescription('The channel to echo into')
-                .setRequired(true))
+        // .addChannelOption(option =>
+        //     option.setName('channel')
+        //         .setDescription('The channel to echo into')
+        //         .setRequired(true))
 	    .addStringOption(option =>
 		    option.setName('input')
-			    .setDescription('The input to echo back'))
-        .addBooleanOption(option =>
-            option.setName('ephemeral')
-            .setDescription('ephemeral or not chooseeee')),
+			    .setDescription('The input to echo back')
+                .setRequired(true)
+                .addChoices(
+                    { name:'funny', value:'You chose funny'},
+                    { name:'Moody', value:'You chose Moody'},
+                )
+                        // .addBooleanOption(option =>
+        //     option.setName('ephemeral')
+        //     .setDescription('ephemeral or not chooseeee'))
+        ),
     async execute(interaction){
         const option = interaction.options
-        await interaction.reply({content:option.getString('input', true), ephemeral:option.getBoolean('ephemeral', false)});
-        await interaction.followUp(option.getChannel('channel'));
+        await interaction.reply(option.getString('input', true))
+        // await interaction.reply({content:option.getString('input', true), ephemeral:option.getBoolean('ephemeral', false)});
+        // await interaction.followUp(option.getChannel('channel'));
     }
 }
