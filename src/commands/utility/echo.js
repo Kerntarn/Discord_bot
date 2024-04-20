462f5ba310297097878c3a376b2f4ea8a3322b47
Kerntarn <fluk032548@gmail.com>
2024-04-20 14:42:37 +0700

    interger option, subcommand

--- src/commands/utility/echo.js
+++ src/commands/utility/echo.js
@@ -13,16 +13,25 @@ module.exports = {
 			    .setDescription('The input to echo back')
                 .setRequired(true)
                 .addChoices(
-                    { name:'funny', value:'You chose funny'},
-                    { name:'Moody', value:'You chose Moody'},
-                )
-                        // .addBooleanOption(option =>
+                    { name:'funny', value:'You chose Funny'},
+                    { name:'Moody', value:'You chose Moody'}))
+        .addIntegerOption(option =>
+            option.setName('count')
+                .setDescription('time repeated of echo')
+                .setMaxValue(5))
+        // .addBooleanOption(option =>
         //     option.setName('ephemeral')
         //     .setDescription('ephemeral or not chooseeee'))
-        ),
+        ,
     async execute(interaction){
         const option = interaction.options
-        await interaction.reply(option.getString('input', true))
+        const count = option.getInteger('count',false)
+        if (count){
+            await interaction.reply(`${option.getString('input', true)} `.repeat(count))
+        }else{
+            await interaction.reply(option.getString('input', true))
+        }
+        
         // await interaction.reply({content:option.getString('input', true), ephemeral:option.getBoolean('ephemeral', false)});
         // await interaction.followUp(option.getChannel('channel'));
     }
